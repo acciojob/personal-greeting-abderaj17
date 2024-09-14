0f71d889232821e5c3f96bb27d89b9cde70a80a4
Adarsh Agrahari <agrahariadarsh68@gmail.com>
2024-09-14 23:01:30 +0530

    adss

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,18 @@
 
-import React from "react";
-import './../styles/App.css';
+import { set } from "cypress/types/lodash";
+import React,{useState} from "react";
 
 const App = () => {
+  const[Text, setText]=useState(0);
+  console.log(Text);
   return (
     <div>
-        {/* Do not remove the main div */}
+        <label htmlFor="ip">Enter your name:</label> <br/>
+        <input id="ip" type="text" onChange={(e)=>{
+          setText(e.target.value)
+        }}/>
+
+        <p>{Text && `Hello {Text}!`}</p>
     </div>
   )
 }
